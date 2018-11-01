5db74310c997afee7c4341cad709040a96a98c5f
Geoffrey Natin <nating@tcd.ie>
2018-11-01 19:03:57 +0000

    Adding hash type separation/file creation code.

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 
 import sys
 import os
+import json
 from infernoball_functions import get_secret, decrypt, read_infernoball
 
 #------------------ SYS ARG ERRORS ------------------
@@ -33,9 +34,47 @@ if(sys.argv[1]=='generate-secret'):
     print(secret)
 
 elif(sys.argv[1]=='generate-next-layer'):
+
+    previous_infernoball = read_infernoball(path_to_infernoball)
+    next_infernoball = json.loads(decrypt(previous_infernoball.get('ciphertext'),secret))
+
+    # Write new infernoball file
     next_layer_folder = sys.argv[4]
-    infernoball = read_infernoball(path_to_infernoball)
-    next_infernoball = json.loads(decrypt(infernoball.get('ciphertext'),secret))
     with open(next_layer_folder+'/infernoball.json', 'w') as outfile:
         json.dump(next_infernoball, outfile)
-        print("Done.")
+
+    # Create new hash files
+    argons = []
+    pbkdf2s = []
+    sha1crypts = []
+    sha512s = []
+    hashes = next_infernoball.get('hashes')
+    for hash in hashes:
+        if hash.startswith('$argon2'):
+            argons.append(hash)
+        elif hash.startswith('$pbkdf2'):
+            pbkdf2s.append(hash)
+        elif hash.startswith('$sha1$'):
+            sha1crypts.append(hash)
+        elif hash.startswith('$6$'):
+            sha512s.append(hash)
+        else:
+            print("Error: Not sure what type of hash %s is." % hash)
+
+    with open(next_layer_folder+'/argon.txt', 'w') as f:
+        for hash in argons:
+            f.write(hash+"\n")
+
+    with open(next_layer_folder+'/PBKDF2.txt', 'w') as f:
+        for hash in pbkdf2s:
+            f.write(hash+"\n")
+
+    with open(next_layer_folder+'/sha1crypt.txt', 'w') as f:
+        for hash in sha1crypts:
+            f.write(hash+"\n")
+
+    with open(next_layer_folder+'/SHA512.txt', 'w') as f:
+        for hash in sha512s:
+            f.write(hash+"\n")
+
+    print("Done.")
